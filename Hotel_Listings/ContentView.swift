333bce3b204c22e456e36c328a8ec2a27542bd4c
tyronec9 <109983232+tyronec9@users.noreply.github.com>
2023-12-25 07:53:27 -0500

    Initial Commit

---
+++ Hotel_Listings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hotel_Listings
+//
+//  Created by Young Khaliq on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hotel_Listings/Hotel_ListingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel_ListingsApp.swift
+//  Hotel_Listings
+//
+//  Created by Young Khaliq on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Hotel_ListingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
